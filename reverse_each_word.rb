d290b17e9e78d27b0bf55b03b86395dbee62a1e7
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-26 04:01:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence)
     sentence.split
     new_sentence = []
-    sentence.split.collect do |backward_words
+    sentence.split.collect do |backward_words|
+      new_sentence
